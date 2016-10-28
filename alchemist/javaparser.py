324219dae5cff7683ef6543129714b4f4f58a798
Wembley Leach <wembley.gl@gmail.com>
2016-10-28 16:20:58 -0400

    Refactor class so JavaParser parses input file

--- alchemist/javaparser.py
+++ alchemist/javaparser.py
@@ -1,6 +1,63 @@
-import alchemist
+from alchemist import Parser
+import re
 
 
-class JavaParser(alchemist.Parser):
-    def __init__(self):
-        super(JavaParser, self).__init__()
+class JavaParser(Parser):
+    def __init__(self, kwargs):
+        self._file_extension = "Java"
+        Parser.__init__(self, kwargs)
+
+    def parse(self):
+        fh = open(self.file)
+        comments = []
+        for line in fh:
+            line = line.strip("\n")
+            match = self.get_class_pattern(line)
+            if match:
+                classname = match.group(1)
+                filename = "{}.{}".format(classname, self._file_extension.lower())
+                self.file = open(filename, "w")
+                if len(comments) > 0:
+                    joined_comments = "\n".join(comments)
+                    print(joined_comments, file=self.file)
+                    comments.clear()
+
+                print("public class {} {{".format(classname), file=self.file)
+                continue
+
+            match = self.get_comment_pattern(line)
+            if match:
+                comments.append(match.group(0))
+                continue
+
+            match = self.get_field_pattern(line)
+            if match:
+                if len(comments) > 0:
+                    joined_comments = "\n".join(comments)
+                    print(joined_comments, file=self.file)
+                    comments.clear()
+
+                print("    {} {};".format(match.group(2), match.group(1)), file=self.file)
+                continue
+
+            match = self.get_end_pattern(line)
+            if match:
+                print("}", file=self.file)
+
+        self.file.close()
+        fh.close()
+
+    def get_comment_pattern(self, line):
+        return re.compile("^[/]{2}\s.*$").search(line)
+
+    def get_class_pattern(self, line):
+        return re.compile("^C\s(([A-Z](?=[a-z])[a-z]+)+)$").search(line)
+
+    def get_field_pattern(self, line):
+        return re.compile("^F\s(\\b(?:[a-z]+)(?=[A-Z]+)(?:[A-Za-z]+)|[a-z]+\\b)\s*((?:[A-Z]?[a-z]+("
+                   "?:[[]])?))$").search(line)
+
+    def get_end_pattern(self, line):
+        return re.compile("^E$").search(line)
+
+
--- alchemist/parser.py
+++ alchemist/parser.py
@@ -1,15 +1,15 @@
 class Parser:
-    def __init__(self, **kwargs):
+    def __init__(self, kwargs={}):
         self._properties = kwargs
 
     def get_properties(self):
         return self._properties
 
     def get_property(self, key):
-        return self._properties.get(key, None);
+        return self._properties.get(key, None)
 
     def set_property(self, key, value):
-        self._properties[key] = value;
+        self._properties[key] = value
 
     @property
     def file(self):
@@ -22,3 +22,17 @@ class Parser:
     @file.deleter
     def file(self):
         del self._properties["file"]
+
+    def get_comment_pattern(self, line):
+        pass
+
+    def get_class_pattern(self, line):
+        pass
+
+    def get_field_pattern(self, line):
+        pass
+
+    def get_end_pattern(self, line):
+        pass
+
+
